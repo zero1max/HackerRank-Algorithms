359d7aa14484422f2c41cf177ce61bec4c38b8b0
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-10-29 21:08:45 +0500

    First Commit

---
+++ algorithms/task5.py
@@ -0,0 +1,8 @@
+n = int(input('Odam sonini kiriting: '))
+
+if n%2 == 0:
+    print(f"{n//2} ta bo'lakka bo'linadi")
+elif n == 1:
+    print("Tortni shundo berasa")
+else:
+    print(f'{n} ta bo\'lakka bo\'linadi')
